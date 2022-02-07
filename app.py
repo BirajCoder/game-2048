d5a0d131b4e0d40691e06fb6db7efe2d2f925199
Biraj De <birajde9@gmail.com>
2022-02-07 21:40:36 +0530

    Final Game

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import random
+import time
 
 class Board:
     grid_colour = (0,0,0)
@@ -9,11 +10,14 @@ class Board:
     WINDOW_HEIGHT = 600
     dim = 4
     blockSize = WINDOW_HEIGHT//dim
+    MAX = 0
     
+
     def __init__(self, parent_window):
         self.parent_window = parent_window
         self.grid = [[0 for i in range(self.dim)] for i in range(self.dim)]
         self.fill_grid()
+
 
     def make_grid(self, n):
         blockSize = self.blockSize
@@ -23,11 +27,14 @@ class Board:
                 pygame.draw.rect(self.parent_window, self.grid_colour, rect, 1)
         pygame.display.flip()
 
+
     def get_row(self, x):
         return self.grid[x-1]
 
+
     def set_row(self, li, x):
         self.grid[x-1] = li
+
 
     def get_column(self, x):
         li = []
@@ -35,16 +42,35 @@ class Board:
             li.append(row[x-1])
         return li
     
+
     def set_column(self, li, x):
         for row in range(self.dim):
             self.grid[row][x-1] = li[row]
 
-    def is_full(self):
+
+    def no_zeros(self):
         for i in range(self.dim):
             for j in range(self.dim):
                 if self.grid[i][j] == 0:
                     return False
         return True
+
+
+    def check_adjacent(self, li):
+        for i in range(self.dim-1):
+            if li[i] == li[i+1]:
+                return True
+        return False
+
+
+    def is_full(self):
+        for i in range(self.dim):
+            if self.check_adjacent(self.get_row(i)):
+                return False
+            if self.check_adjacent(self.get_column(i)):
+                return False
+        return True
+
 
     def update_list(self, li, reverse=False):
         if reverse:
@@ -66,6 +92,8 @@ class Board:
                 
             if li[ptr1] == li[ptr2]:
                 li[ptr2] = 2 * li[ptr2]
+                # Set Max value
+                self.MAX = max(self.MAX, li[ptr2])
                 li[ptr1] = 0
                 ptr1-=1
                 ptr2-=1
@@ -76,13 +104,16 @@ class Board:
         if reverse:
             return li[::-1]
         return li
-        
-    def write_text(self, text, location, color=(0,0,0)):
-        textFont = pygame.font.Font('freesansbold.ttf', 30)
+
+
+    def write_text(self, text, location, color=(0,0,0), size=35):
+        textFont = pygame.font.Font('freesansbold.ttf', size)
         textSurf = textFont.render(text, True, color)
         textRect = textSurf.get_rect()
         textRect.center = location
         self.parent_window.blit(textSurf, textRect)
+        pygame.display.flip()
+
 
     def fill_grid(self):
         text_loc = self.blockSize
@@ -96,38 +127,45 @@ class Board:
                     self.write_text(str(self.grid[x][y]), (loc_x, loc_y))
                 loc_x += text_loc
             loc_y += text_loc
-        print(*self.grid, sep="\n")
         pygame.display.flip()
+
 
     def get_random_index(self):
         n = random.randrange(0, self.dim * self.dim)
         return (n//self.dim, n%self.dim)
 
+
     def fill_random(self):
         i, j = self.get_random_index()
-        print(i,j, self.grid[i][j] != 0)
         while self.grid[i][j] != 0:
             i, j = self.get_random_index()
         self.grid[i][j] = random.choice([2, 4])
 
 
-    def move_left(self):
+    def move_horizontal(self, reverse=False):
+        flag = False
         for i in range(self.dim):
-            self.set_row(self.update_list(self.get_row(i), True), i)
+            previous = self.get_row(i)
+            updated = self.update_list(previous, reverse)
+            self.set_row(updated, i)
+            if previous != updated:
+                flag=True
+        return flag
 
-    def move_right(self):
-        for i in range(self.dim):
-            self.set_row(self.update_list(self.get_row(i)), i)
 
-    def move_up(self):
+    def move_vertical(self, reverse=False):
+        flag = False
         for i in range(self.dim):
-            self.set_column(self.update_list(self.get_column(i), True), i)
+            previous = self.get_column(i)
+            updated = self.update_list(previous, reverse)
+            self.set_column(updated, i)
+            if previous != updated:
+                flag=True
+        return flag
 
-    def move_down(self):
-        for i in range(self.dim):
-            self.set_column(self.update_list(self.get_column(i)), i)
 
 class Game:
+    MAX = 2048
 
     def __init__(self):
         pygame.init()
@@ -145,28 +183,38 @@ class Game:
                 if event.type == KEYDOWN:
                     if start:
                         self.board.fill_random()
-                    self.board.fill_random()
+                        self.board.fill_random()
                     start = False
                     if event.key == K_ESCAPE:
                         running = False
-                        
+
                     if event.key == K_LEFT:
-                        self.board.move_left()
+                        self.board.move_horizontal(True)
 
                     if event.key == K_RIGHT:
-                        self.board.move_right()
+                        self.board.move_horizontal()
 
                     if event.key == K_UP:
-                        self.board.move_up()
+                        self.board.move_vertical(True)
 
                     if event.key == K_DOWN:
-                        self.board.move_down()
+                        self.board.move_vertical()
+
                     self.board.fill_grid()
-                    if self.board.is_full():
+                    
+                    # Maximum score achieved 
+                    if self.MAX <= self.board.MAX:
+                        running = False
+                    has_no_zero = self.board.no_zeros()
+                    if not has_no_zero:
+                        self.board.fill_random()
+                    if self.board.is_full() and has_no_zero:
                         running = False
                 elif event.type == QUIT:
                     running = False
-                
+                if running == False:
+                    self.board.write_text(f"GAME OVER.. Please wait", (300, 650), (255, 0, 0), 20)
+                    time.sleep(4)
 
 
 if __name__ == "__main__":
